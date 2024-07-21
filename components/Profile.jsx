4502548c182e6fb43756c718d36a2280d4e65d19
henry edet <ghost@henrys-MacBook-Air.local>
2024-07-21 23:00:52 +0300

    image section

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -3,20 +3,44 @@
 import { motion } from "framer-motion";
 import Image from "next/image";
 import henry from "@/public/newFine.png";
+import { FaInstagram } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa6";
+import "@/components/Profile-gsap";
 
 const Photo = () => {
   return (
     <div className="w-full h-full relative">
       <motion.div>
-        <div className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px]">
-          <Image
-            src={henry.src}
-            alt="profile photo"
-            fill
-            priority
-            quality={100}
-            className="object-contain"
-          />
+        <div className="person">
+          <div className="person__container">
+            <div className="avatar">
+              <Image
+                src={henry.src}
+                alt="profile photo"
+                fill
+                priority
+                quality={100}
+                className="profile-image"
+              />
+            </div>
+            <div className="person__details">
+              <h1>Engr. Henry Edet</h1>
+            </div>
+            <div className="person__role">
+              Software Engineer & Cybersecurity Analyst
+            </div>
+
+            <div className="card-footer">
+              <div className="social-buttons">
+                <button>
+                  <FaInstagram />
+                </button>
+                <button>
+                  <FaGithub />
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
       </motion.div>
     </div>
@@ -24,5 +48,3 @@ const Photo = () => {
 };
 
 export default Photo;
-
-// className="profile-photo" initial={{ opacity: 0 }} animate={{ opacity: 1 }}
--- components/Section.jsx
+++ components/Section.jsx
@@ -12,7 +12,13 @@ const Section = () => {
         <h2>About</h2>
         <p>
           I am a software engineer and cybersecurity analyst with a passion for
-          learning and problem solving.
+          learning and problem solving; with 2 years of experience in designing
+          software architecture and enterprise level solutions. Demonstrated
+          success in mentoring engineers, reducing product delivery time, and
+          managing micro services across diverse technologies. Able to
+          effectively self-manage during independent projects. Exceptional
+          communication and interpersonal skills with expertise in building,
+          managing and scaling high performance technology organizations.
         </p>
       </div>
     </section>
