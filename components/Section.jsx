168c7c94a10e1f64c778cc493bbf9de98be816a4
henry edet <ghost@henrys-MacBook-Air.local>
2024-07-24 16:15:07 +0300

    animated text

--- components/Section.jsx
+++ components/Section.jsx
@@ -1,6 +1,8 @@
 import "@/components/style.css";
 import Profile from "@/components/Profile";
 // import SplitText from "@/components/Textanimate";
+import { motion } from "framer-motion";
+import AnimatedText from "./AnimatedText.js";
 
 const Section = () => {
   return (
@@ -9,19 +11,10 @@ const Section = () => {
       className="backgroundImg grid grid-cols-2 justify-center items-center"
     >
       <Profile />
-      <div className="text-white about-me">
-        <h2>About Me</h2>
-        <p>
-          I am a software engineer and cybersecurity analyst with a passion for
-          learning and problem solving; with 2 years of experience in designing
-          software architecture and enterprise level solutions. Demonstrated
-          success in mentoring engineers, reducing product delivery time, and
-          managing micro services across diverse technologies. Able to
-          effectively self-manage during independent projects. Exceptional
-          communication and interpersonal skills with expertise in building,
-          managing and scaling high performance technology organizations.
-        </p>
-      </div>
+      <motion.div className="text-white about-me">
+        <AnimatedText text="animated text with framer-motion" />
+        <AnimatedText text="animated text with framer-motion" />
+      </motion.div>
     </section>
   );
 };
